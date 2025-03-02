d9e405f7cd3d7061e7c1dfe1874c4e65f2577b05
Siim Raudsepp <siimraudsepp@msn.com>
2025-03-03 01:37:27 +0200

    Cleanup and release prep

--- DropdownNextPrev/DropdownNextPrev/AddDropdownPatch.cs
+++ DropdownNextPrev/DropdownNextPrev/AddDropdownPatch.cs
@@ -83,12 +83,12 @@ public static class AddDropdownPatch
         var dropdown = new GuiElementDropdownWithNavigation(composer.Api, values, names, selectedIndex,
             onSelectionChanged,
             bounds, font);
-        var leftArrowBounds = ElementBounds.Fixed(0 - 23, 0, 20, bounds.fixedHeight);
-        var rightArrowBounds = ElementBounds.Fixed(bounds.fixedWidth + 5, 0, 20, bounds.fixedHeight);
+        var leftArrowBounds = ElementBounds.Fixed(0 - 22, 0, 20, bounds.fixedHeight);
+        var rightArrowBounds = ElementBounds.Fixed(bounds.fixedWidth + 2, 0, 20, bounds.fixedHeight);
 
         var buttonFont = CairoFont.WhiteMediumText().WithWeight(FontWeight.Bold)
             .WithOrientation(EnumTextOrientation.Center).WithFontSize(20);
-        var buttonStyle = EnumButtonStyle.Normal;
+        var buttonStyle = EnumButtonStyle.None;
         composer.AddInteractiveElement(
                 dropdown, key).BeginChildElements()
             .AddButton("<", dropdown.OnLeftArrowClick, leftArrowBounds, buttonFont, buttonStyle)
